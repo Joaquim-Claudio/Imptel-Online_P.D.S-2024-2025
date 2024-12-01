695f366a40becec959cfb36435684d33e7d75b3a
Joaquim <joaquim.claudio2003@gmail.com>
2024-12-01 20:50:15 +0000

    Fixed redis connection issues.

---
+++ src/services/docker-compose/redis/10-initial.sh
@@ -0,0 +1,11 @@
+export REDISCLI_AUTH=Imp7e1_db_session
+
+if [ "$(redis-cli -h 127.0.0.1 -p 6379 DBSIZE)" -eq 0 ]; then
+
+    redis-cli -h 127.0.0.1 -p 6379 "HMSET" "CURRENT_ACAD_YEAR" "absexp" "-1" "sldexp" "-1" "data" "2024"
+    redis-cli -h 127.0.0.1 -p 6379 "HMSET" "ACAD_YEAR_OPEN" "absexp" "-1" "sldexp" "-1" "data" "true"
+    redis-cli -h 127.0.0.1 -p 6379 "HMSET" "NEXT_STUDENT_SUFIX" "absexp" "-1" "sldexp" "-1" "data" "0001"
+    redis-cli -h 127.0.0.1 -p 6379 "HMSET" "NEXT_TEACHER_SUFIX" "absexp" "-1" "sldexp" "-1" "data" "0001"
+    redis-cli -h 127.0.0.1 -p 6379 "HMSET" "NEXT_SECRETARY_SUFIX" "absexp" "-1" "sldexp" "-1" "data" "0001"
+    redis-cli -h 127.0.0.1 -p 6379 "HMSET" "NEXT_HELPDESK_SUFIX" "absexp" "-1" "sldexp" "-1" "data" "0001"
+fi
