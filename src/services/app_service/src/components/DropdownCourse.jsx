888f5afa15f3209d5b5f2088cf6f4acebcfecf32
Edja da Silva <tamaramonteiro2014@icloud.com>
2024-12-01 17:57:11 +0000

    page StudentList

--- src/services/app_service/src/components/DropdownCourse.jsx
+++ src/services/app_service/src/components/DropdownCourse.jsx
@@ -1,26 +1,47 @@
 import React, { useState } from "react";
+import dropdownIcon from "../assets/images/fi-rr-caret-right.svg"
 
 function DropdownCourse({ course, grades }) {
     const [isOpen, setIsOpen] = useState(false);
+    const [openClassKey, setOpenClassKey]= useState(null);
+
+    const toogleClassKeyDropdown= (classKey)=>{
+        if(openClassKey===classKey){
+            setOpenClassKey(null);
+        }else{
+            setOpenClassKey(classKey);
+        }
+
+    }
+
 
     
 
     return (
         <div>
-            <button onClick={() => setIsOpen(!isOpen)}>{course}</button>
+            <button className="dropdown-btn" onClick={() => setIsOpen(!isOpen)}> <img className="pe-3" src={dropdownIcon} /> {course}</button>
             {isOpen && (
                 <div>
                     {Object.keys(grades).map((classKey) => {
                         const [courseIn, grade, group] = classKey.split('-');
                         return (
                             <div key={classKey}>
-                                <h4>{`${courseIn}${grade}${group}`}</h4>
-                                
-                                    {grades[classKey].map((student) => (
-                                        <div key={student.id}>
+                                <button className="dropdownClassKey " onClick={() =>toogleClassKeyDropdown(classKey)} > <img className="pe-3" src={dropdownIcon} />
+                                {`${courseIn}${grade}${group}`}
+                                </button  >
+
+                                {openClassKey==classKey &&
+                                (grades[classKey].map((student) => (
+                                        <ul className="dropdownClasskey-item" key={student.id}>
+                                            <li >
                                             {student.name} {student.internId}
-                                        </div>
-                                    ))}
+                                            </li>                                            
+                                        </ul>
+                                    )))}
+                                
+                                
+                                
+                                    
                             
                             </div>
                         );
